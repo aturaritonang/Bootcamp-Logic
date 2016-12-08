f90d57eabff4886103722bbbbeeb63e745fe753e
Atur Aritonang <mangatur.aritonang@gmail.com>
2016-12-08 08:15:26 +0700

    Add exam day 7
    Add new solution for day 7 of exam.

---
+++ AppLatihanLogic07/AppLatihanLogic07/BasicLogic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLatihanLogic07
+{
+    public class BasicLogic
+    {
+        public BasicLogic()
+        {
+
+        }
+        public int nilaiTinggi { get; set; }
+        public int nilaiLebar { get; set; }
+        public string[,] array2D { get; set; }
+    }
+}
---
+++ AppLatihanLogic07/AppLatihanLogic07/Soal01.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLatihanLogic07
+{
+    public class Soal01
+    {
+    }
+}
